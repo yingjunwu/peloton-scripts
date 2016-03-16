1bf75becaf769d278363f41e2069159cf021b0d9
Yingjun Wu <wu.yj0616@gmail.com>
2016-03-15 20:05:50 -0400

    update measure performance

--- measure-performance-ycsb.py
+++ measure-performance-ycsb.py
@@ -3,6 +3,7 @@
 # date: March 5th, 2016
 
 import os
+import sys
 import time
 
 server1_name = "yingjunw@dev1.db.pdl.cmu.local"
@@ -14,10 +15,10 @@ parameters = {
 "$IP":  "localhost",
 "$PORT": "57721",
 "$SCALE_FACTOR": "1",
-"$TIME":  "10",
+"$TIME":  "120",
 "$THREAD_NUMBER": "1",
-"$READ_RATIO": "50",
-"$INSERT_RATIO": "50",
+"$READ_RATIO": "0",
+"$INSERT_RATIO": "0",
 "$SCAN_RATIO": "0",
 "$UPDATE_RATIO": "0",
 "$DELETE_RATIO": "0",
@@ -30,13 +31,13 @@ start_cleanup_script = "rm -rf callgrind.out.*"
 start_peloton_valgrind_script = "valgrind --tool=callgrind --trace-children=yes ./src/peloton -D ./data > /dev/null 2>&1 &"
 start_peloton_script = "./src/peloton -D ./data > /dev/null 2>&1 &"
 stop_peloton_script = "pg_ctl -D ./data stop"
-start_ycsb_bench_script = "./oltpbenchmark -b ycsb -c " + cwd + "/" + config_filename + " --create=true --load=false --execute=true -s 5 -o outputfile"
+start_ycsb_bench_script = "./oltpbenchmark -b ycsb -c " + cwd + "/" + config_filename + " --create=true --load=true --execute=true -s 5 -o outputfile"
 
-def prepare_parameters(thread_num, read_ratio):
-    os.chdir(cwd)
+def prepare_parameters(thread_num, read_ratio, insert_ratio, update_ratio):
     parameters["$THREAD_NUMBER"] = str(thread_num)
     parameters["$READ_RATIO"] = str(read_ratio)
-    parameters["$INSERT_RATIO"] = str(100-read_ratio)
+    parameters["$INSERT_RATIO"] = str(insert_ratio)
+    parameters["$UPDATE_RATIO"] = str(update_ratio)
     ycsb_template = ""
     with open("ycsb_template.xml") as in_file:
         ycsb_template = in_file.read()
@@ -46,35 +47,40 @@ def prepare_parameters(thread_num, read_ratio):
         out_file.write(ycsb_template)
         
 def start_peloton_valgrind():
-    os.chdir(cwd)
     os.system(stop_peloton_script)
     os.system(start_cleanup_script)
     os.system(start_peloton_valgrind_script)
     time.sleep(5)
 
 def start_peloton():
-    os.chdir(cwd)
     os.system(stop_peloton_script)
     os.system(start_cleanup_script)
     os.system(start_peloton_script)
     time.sleep(5)
 
-def start_bench(thread_num, read_ratio):
+def start_bench(thread_num, read_ratio, insert_ratio, update_ratio):
     # go to oltpbench directory
     os.chdir(os.path.expanduser(oltp_home))
-    os.system(start_ycsb_bench_script + "_t" + str(thread_num) + "_" + str(read_ratio))
-    time.sleep(2)
+    os.system(start_ycsb_bench_script + "_t" + str(thread_num) + "_" + str(read_ratio) + "_" + str(insert_ratio) + "_" + str(update_ratio))
 
 def stop_peloton():
     # go back to cwd
     os.chdir(cwd)
     os.system(stop_peloton_script)
 
+def collect_data(thread_num, read_ratio, insert_ratio, update_ratio):
+    os.chdir(cwd)
+    dir_name = "collected_data_t" + str(thread_num) + "_" + str(read_ratio) + "_" + str(insert_ratio) + "_" + str(update_ratio)
+    os.system("rm -rf " + dir_name)
+    os.system("mkdir " + dir_name)
+    os.system("mv callgrind.out.* " + dir_name)
+
 if __name__ == "__main__":
+    read_ratio = 0
+    insert_ratio = 100
+    update_ratio = 0
     start_peloton()
-    for thread_num in range(1, 3):
-        for read_ratio in range(0, 1, 10):
-            prepare_parameters(thread_num, read_ratio)
-            start_bench(thread_num, read_ratio)
+    for thread_num in range(1, 6):
+        prepare_parameters(thread_num, read_ratio, insert_ratio, update_ratio)
+        start_bench(thread_num, read_ratio, insert_ratio, update_ratio)
     stop_peloton()
-    print("the end.......")
