231c19441d437863bb0480e72ee0d2e5f735938b
Yingjun <yingjun@comp.nus.edu.sg>
2016-10-07 11:46:01 +0800

    update script

--- measure-performance-ycsb.py
+++ measure-performance-ycsb.py
@@ -25,9 +25,15 @@ parameters = {
 
 cwd = os.getcwd()
 config_filename = "peloton_ycsb_config.xml"
+
+start_cleanup_script = "rm -rf callgrind.out.*"
+
 start_peloton_script = "./bin/peloton -port " + peloton_port + " > /dev/null 2>&1 &"
+start_peloton_valgrind_script = "valgrind --tool=callgrind --trace-children=yes " + start_peloton_script
+
 stop_peloton_script = "pkill -9 peloton"
-start_ycsb_bench_script = "./oltpbenchmark -b ycsb -c " + cwd + "/" + config_filename + " --create=true --load=true --execute=true -s 5 -o outputfile | grep requests/sec"
+
+start_ycsb_bench_script = "./oltpbenchmark -b ycsb -c " + cwd + "/" + config_filename + " --create=true --load=true --execute=true -s 5 -o outputfile | grep requests/sec >> peloton_result"
 
 def prepare_parameters(thread_num, read_ratio, insert_ratio, update_ratio):
     os.chdir(cwd)
@@ -43,17 +49,20 @@ def prepare_parameters(thread_num, read_ratio, insert_ratio, update_ratio):
     with open(config_filename, "w") as out_file:
         out_file.write(ycsb_template)
 
-
-def start_peloton():
+def start_peloton(is_profiling):
     os.chdir(cwd)
     os.system(stop_peloton_script)
-    os.system(start_peloton_script)
-    time.sleep(5)
+    os.system(start_cleanup_script)
+    if is_profiling:
+        os.system(start_peloton_valgrind_script)
+    else:
+        os.system(start_peloton_script)
+    time.sleep(2)
 
 def start_bench(thread_num, read_ratio, insert_ratio, update_ratio):
     # go to oltpbench directory
     os.chdir(os.path.expanduser(oltp_home))
-    os.system(start_ycsb_bench_script + "_t" + str(thread_num) + "_" + str(read_ratio) + "_" + str(insert_ratio) + "_" + str(update_ratio))
+    os.system(start_ycsb_bench_script) + "_t" + str(thread_num) + "_" + str(read_ratio) + "_" + str(insert_ratio) + "_" + str(update_ratio)
     time.sleep(2)
 
 def stop_peloton():
@@ -62,15 +71,24 @@ def stop_peloton():
     os.system(stop_peloton_script)
 
 if __name__ == "__main__":
-    read_ratio = 100
-    insert_ratio = 0
-    update_ratio = 0
-    start_peloton()
+    if (len(sys.argv) != 6):
+        print("usage: " + sys.argv[0] + " is_profiling thread_num read_ratio insert_ratio update_ratio")
+        exit(0)
+    
+    is_profiling = int(sys.argv[1])
+    thread_num = int(sys.argv[2])
+    read_ratio = int(sys.argv[3])
+    insert_ratio = int(sys.argv[4])
+    update_ratio = int(sys.argv[5])
+    
+    
+    print("thread_num = " + str(thread_num))
+    print("read_ratio = " + str(read_ratio))
+    print("insert_ratio = " + str(insert_ratio))
+    print("update_ratio = " + str(update_ratio))
 
-    prepare_parameters(1, read_ratio, insert_ratio, update_ratio)
-    start_bench(1, read_ratio, insert_ratio, update_ratio)
+    prepare_parameters(thread_num, read_ratio, insert_ratio, update_ratio)
 
-    for thread_num in range(8, 41, 8):
-        prepare_parameters(thread_num, read_ratio, insert_ratio, update_ratio)
-        start_bench(thread_num, read_ratio, insert_ratio, update_ratio)
+    start_peloton(is_profiling)
+    start_bench(thread_num, read_ratio, insert_ratio, update_ratio)
     stop_peloton()
